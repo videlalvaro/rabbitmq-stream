4cc14ec356235bf5993eb98f009bec81e4c8a410
Alvaro Videla <videlalvaro@gmail.com>
2013-10-07 21:34:45 -0500

    adds mgmt part to build process

--- package.mk
+++ package.mk
@@ -1,1 +1,6 @@
-DEPS:=rabbitmq-erlang-client rabbitmq-consistent-hash-exchange rabbitmq-management
+DEPS:=rabbitmq-erlang-client rabbitmq-consistent-hash-exchange rabbitmq-management random-exchange
+
+CONSTRUCT_APP_PREREQS:=$(shell find $(PACKAGE_DIR)/priv -type f)
+define construct_app_commands
+	cp -r $(PACKAGE_DIR)/priv $(APP_DIR)
+endef
