8d502f50054787e210a6a1d0d15d128ebcf21e13
Alvaro Videla <videlalvaro@gmail.com>
2013-10-13 23:57:53 -0500

    fixes scripts

--- etc/test.sh
+++ etc/test.sh
@@ -3,4 +3,4 @@ CTL=$1
 
 $CTL set_parameter stream-connection-params localhost '{"uri": "amqp://localhost"}'
 $CTL set_parameter stream-definition my_stream '{"connection-params": "localhost", "shards-per-node": 4}'
-$CTL set_policy my-stream   "^stream\."   '{"stream-definition": "my_stream"}'
+$CTL set_policy my_stream   "^stream\."   '{"stream-definition": "my_stream"}'
--- etc/three-queues.sh
+++ etc/three-queues.sh
@@ -3,4 +3,4 @@ CTL=$1
 
 $CTL set_parameter stream-connection-params localhost '{"uri": "amqp://localhost"}'
 $CTL set_parameter stream-definition 3_stream '{"connection-params": "localhost", "shards-per-node": 3}'
-$CTL set_policy my-stream   "^three\."   '{"stream-definition": "3_stream"}'
+$CTL set_policy 3_stream   "^three\."   '{"stream-definition": "3_stream"}'
