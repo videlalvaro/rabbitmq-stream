2372af152ccf180cd9cbd3385e9a6a38090494e7
Alvaro Videla <videlalvaro@gmail.com>
2013-10-13 23:58:06 -0500

    fixes script

--- etc/rkey.sh
+++ etc/rkey.sh
@@ -2,5 +2,5 @@
 CTL=$1
 
 $CTL set_parameter stream-connection-params localhost '{"uri": "amqp://localhost", "routing-key": "1234"}'
-$CTL set_parameter stream-definition rkey '{"connection-params": "localhost", "shards-per-node": 3}'
-$CTL set_policy my-stream   "^rkey\."   '{"stream-definition": "rkey"}'
+$CTL set_parameter stream-definition rkey '{"connection-params": "localhost", "shards-per-node": 2}'
+$CTL set_policy rkey-stream   "^rkey\."   '{"stream-definition": "rkey"}'
