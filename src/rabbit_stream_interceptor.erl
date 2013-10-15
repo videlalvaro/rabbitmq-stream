b2b928ec19962d25f04792932d38e8afeed547ba
Alvaro Videla <videlalvaro@gmail.com>
2013-10-15 00:45:25 -0500

    implements queue unbind intercept function

--- src/rabbit_stream_interceptor.erl
+++ src/rabbit_stream_interceptor.erl
@@ -34,6 +34,9 @@ intercept('queue_declare', _Q1, Q2) ->
 intercept('queue_bind', _Q1, Q2) ->
     {error, rabbit_misc:format("Can't bind sharded queue: ~p", [Q2])};
 
+intercept('queue_unbind', _Q1, Q2) ->
+    {error, rabbit_misc:format("Can't unbind sharded queue: ~p", [Q2])};
+
 intercept(_Other, _Q1, Q2) ->
     {ok, Q2}.
 
